526ccb13b5e9befb3085dc6f89346af00d422e22
aiegoo <eozz21@gmail.com>
2019-01-15 20:28:27 +0900

    missing parts from the 2019 02 files

--- js/2019.02.js
+++ js/2019.02.js
@@ -1,10 +1,21 @@
 const log = consle.log;
-
+var pos = $("#box1").css("margin-top");
+var tar = Number(pos.replace("px", "")) + 200;
+log(pos, tar);
 $("#bt1").click(function(){
 /* 	$("#box1").stop().fadeToggel(1000); */
-
+/* 
 	$("#box1").animate({
 			marginTop: '9rem',
 			opacity: '0',
+	} */
+
+$("#bt1").click(function(){
+	if($("#box1").css("margin-top") == pos) {
+		$("#box1").stop().animate({"margin-top":tar+"px"});
 	}
+	else {
+		$("#box1").stop().animate({"margin-top":pos});
+	}
+});
 });
